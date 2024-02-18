3059bf7298e1291a630da74585f5b3d6f296a4b3
CtrlZGau <gautham@Gauthams-MacBook-Air.local>
2024-02-18 13:11:44 +0530

    edited the task2

--- Payload/payload.py
+++ Payload/payload.py
@@ -1,20 +1,43 @@
 import cv2
 import numpy as np
+def resize_image(img, binning_factor):
+    # Get the input image dimensions
+    height, width = img.shape[:2]
 
-img = cv2.imread('unknown.jpeg')
-window_name = 'image'
-img_blur = cv2.blur (img, (3,3))
+    # Compute the dimensions of the resized image
+    resized_height = (height // binning_factor) * binning_factor
+    resized_width = (width // binning_factor) * binning_factor
 
+    # Resize the image
+    resized_img = cv2.resize(img, (resized_width, resized_height))
+    return resized_img
 
-cv2.imshow(window_name,img)
+def pixel_binning(img, binning_factor):
+    # Get the input image dimensions
+    height, width = img.shape[:2]
 
+    # Compute the dimensions of the binned image
+    binned_height = height // binning_factor
+    binned_width = width // binning_factor
 
-cv2.waitKey(0) 
-  
-# closing all open windows 
-cv2.destroyAllWindows()
+    # Reshape the image to a 2D array of pixels in each bin
+    reshaped_img = img.reshape(binned_height, binning_factor,
+                               binned_width, binning_factor, -1)
 
+    # Compute the average for each bin
+    binned_img = reshaped_img.mean(axis=(1, 3))
 
+    return binned_img.astype(np.uint8)
 
+# Load the image
+img = cv2.imread('clock_tower.jpeg')
 
+# Resize the image
+img = resize_image(img, 4)
+
+# Perform pixel binning with a binning factor of 4
+binned_img = pixel_binning(img, 4)
+
+# Save the binned image
+cv2.imwrite('clock_tower_binned.jpg', binned_img)
 
--- Payload/payload2.py
+++ Payload/payload2.py
@@ -1,11 +1,13 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error,r2_score
 
 iris = load_iris()
 
+# setting yp the data for training
 Y = np.asarray(iris.target)
 
 X= np.asarray(iris.data)
@@ -14,11 +16,14 @@ X = np.delete(X,2,axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2,random_state=50)
 
+# Initialising the model
 model = LogisticRegression()
 model.fit(X_train,y_train)
 
+#Prediction
 y_predicted = model.predict(X_test)
 
+# Evaluation of training model
 mse = mean_squared_error(y_test,y_predicted)
 r2 = r2_score(y_test,y_predicted)
 print("Mean Squared Error: ",mse)
